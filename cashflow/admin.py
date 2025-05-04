5e135dce7474907b8fdc7f05c771a65016881feb
VasyaPupkin120 <VPupkinza@yandex.ru>
2025-05-05 00:54:35 +0700

    подробные имена в моделях, классы-редакторы для админки

--- cashflow/admin.py
+++ cashflow/admin.py
@@ -1,8 +1,73 @@
 from django.contrib import admin
+from django import forms
 
-from .models import Category, StatusFlow, TypeFlow, CashFlow 
+from .models import TypeFlow, Category, Subcategory, StatusFlow, CashFlow 
 
-admin.site.register(Category)
-admin.site.register(StatusFlow)
-admin.site.register(TypeFlow)
-admin.site.register(CashFlow)
+
+################################################################################
+# классы-инлинеры
+################################################################################
+class CashFlowInline(admin.TabularInline):
+    """
+    Отображение связанных записей движений
+    """
+    model = CashFlow
+    fields = ('amount', 'typeflow', 'category', 'subcategory', 'status', 'created_at', 'comment')
+    readonly_fields = ('amount', 'typeflow', 'category', 'subcategory', 'status', 'created_at', 'comment')
+    extra = 0
+
+
+class CategoryInline(admin.TabularInline):
+    """
+    Для отображения связанных категорий
+    """
+    model = Category
+    extra = 1
+
+
+class SubcategoryInline(admin.TabularInline):
+    """
+    Для отображения связанных подкатегорий
+    """
+    model = Subcategory
+    extra = 1
+
+################################################################################
+# классы-редакторы
+################################################################################
+
+class TypeFlowAdmin(admin.ModelAdmin):
+    inlines = (CategoryInline,)
+    list_display = ['name']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (SubcategoryInline,)
+    list_display = ['name', 'typeflow'] 
+    
+
+class SubcategoryAdmin(admin.ModelAdmin):
+    inlines = (CashFlowInline, )
+    list_display = ['name', 'supercategory'] 
+
+
+class StatusFlowAdmin(admin.ModelAdmin):
+    inlines = (CashFlowInline,)
+    list_display = ['name']
+
+
+class CashFlowAdmin(admin.ModelAdmin):
+    list_display = ['amount', 'typeflow', 'category', 'subcategory', 'status', 'created_at', 'comment']
+    widgets = {
+        'comment': forms.Textarea(attrs={'rows': 2, 'cols': 20}),
+    }
+
+################################################################################
+# регистрация
+################################################################################
+
+admin.site.register(TypeFlow, TypeFlowAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Subcategory, SubcategoryAdmin)
+admin.site.register(StatusFlow, StatusFlowAdmin)
+admin.site.register(CashFlow, CashFlowAdmin)
--- cashflow/models.py
+++ cashflow/models.py
@@ -1,39 +1,81 @@
 from django.db import models
 from django.utils import timezone
+from django.core.exceptions import ValidationError
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
-    typeflow = models.ForeignKey('TypeFlow', null=True, blank=True, on_delete=models.SET_NULL, related_name='categories')
-    supercategory = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='subcategories')
+    name = models.CharField(max_length=50, verbose_name="Название")
+    typeflow = models.ForeignKey('TypeFlow', on_delete=models.PROTECT, related_name='categories', verbose_name="Тип движения")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
     def __str__(self):
         return self.name
+
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название")
+    supercategory = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='subcategories', verbose_name="Надкатегория")
+
+    class Meta:
+        verbose_name = "Подкатегория"
+        verbose_name_plural = "Подкатегории"
+
+    def __str__(self):
+        return self.name
+
 
 class StatusFlow(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, verbose_name="Название")
+
+    class Meta:
+        verbose_name = "Статус"
+        verbose_name_plural = "Статусы"
 
     def __str__(self):
         return self.name
+
 
 class TypeFlow(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, verbose_name="Название")
+
+    class Meta:
+        verbose_name = "Тип"
+        verbose_name_plural = "Типы"
 
     def __str__(self):
         return self.name
 
-class CashFlow(models.Model):
-    amount = models.DecimalField(max_digits=30, decimal_places=2)
-    comment = models.TextField(null=True, blank=True)
-    created_at = models.DateTimeField(editable=True)
 
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL, related_name="cashflows")
-    status = models.ForeignKey('StatusFlow', null=True, blank=True, on_delete=models.SET_NULL, related_name="cashflows")
-    typeflow = models.ForeignKey('TypeFlow', null=True, blank=True, on_delete=models.SET_NULL, related_name="cashflows")
+class CashFlow(models.Model):
+    amount = models.DecimalField(max_digits=30, decimal_places=2, verbose_name="Сумма")
+    comment = models.TextField(null=True, blank=True, verbose_name="Комментарий")
+    created_at = models.DateTimeField(editable=True, verbose_name="Дата движения")
+    typeflow = models.ForeignKey('TypeFlow', on_delete=models.PROTECT, related_name="cashflows", verbose_name="Тип движения")
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, related_name="cashflows", verbose_name="Категория")
+    subcategory = models.ForeignKey('Subcategory', on_delete=models.PROTECT, related_name="cashflows", verbose_name="Подкатегория")
+    status = models.ForeignKey('StatusFlow', on_delete=models.PROTECT, related_name="cashflows", verbose_name="Статус")
+    
+    class Meta:
+        verbose_name = "Движение средств"
+        verbose_name_plural = "Движения средств"
+        ordering = ["-created_at"]
 
     def __str__(self):
         return str(self.amount)
 
+    def clean(self):
+        super().clean()
+        if self.category.typeflow != self.typeflow:
+            raise ValidationError({'category': f"Категория {self.category.name} не принадлежит типу {self.typeflow.name}"})
+        if self.subcategory.supercategory != self.category:
+            raise ValidationError({'subcategory': f"Подкатегория {self.subcategory.name} не принадлежит категории {self.category.name}"})
+
     def save(self, *args, **kwargs):
+        # для запуска валидации в любом случае
+        self.full_clean()
         # для автозаполнения времени создания + сохранения возможности редактирования
         if not self.pk:
             self.created_at = timezone.now()
